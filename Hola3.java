b6305b0d62764be5e00f9972f92eaa7aeae9ff1c
Daniel Suarezx <juan.daniel.suarez@hotmail.com>
2020-02-19 19:04:08 -0500

    mundo mundial

--- Hola3.java
+++ Hola3.java
@@ -5,7 +5,7 @@ public class Hola3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola Tercer Mundo");
+		System.out.println("Hola Tercer Mundo mundial");
 	}
 
 }
